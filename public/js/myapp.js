a038198b34a89a1ee43db1d91fd480366dc9e25f
jahnava <jahnavalmt@gmail.com>
2018-03-05 13:05:51 -0500

    ran through js loop and changed box color

--- public/js/myapp.js
+++ public/js/myapp.js
@@ -1,12 +1,14 @@
 function set()
 {
-  //get all elements with the id "cssProperty" (all the dropdowns) and log them out in the console.
+  //gets all elements with the id "cssProperty" (all the dropdowns) and log them out in the console.
   let elements =
   document.getElementsByName('cssProperty');
 
+//loops through the index to grab each value from the elements array
   for( let index = 0; index < elements.length; index++
   )
   {
+    //gets the attribute by the id name from "cssProperty"
     let cssProperty = elements[ index ].getAttribute('id');
 
     let cssValue = elements[ index ].value;
